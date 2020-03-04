20f430970ed2f4c4eb452be3754052e7774bc49a
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-04 00:24:44 -0300

    Creating and listing tasks

--- API/src/routes.js
+++ API/src/routes.js
@@ -4,14 +4,17 @@ import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-// Todas as rotats abaixo deste middleware precisam estar autenticadas
+// Todas as rotas abaixo deste middleware precisam estar autenticadas
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
+routes.post('/tasks', TaskController.store);
+routes.get('/tasks', TaskController.index);
 export default routes;
